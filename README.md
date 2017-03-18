153f4552e21b2e6468d6516323c2bab3bea4cd7b
Neel Patel <npatel@Neels-MacBook-Pro.local>
2017-03-18 18:15:10 +1100

    Attempted to upgrade links and scripts

---
+++ README.md
@@ -0,0 +1,15 @@
+# Easy List
+
+Simple list application.
+
+# Usage
+
+```
+npm install
+```
+
+```
+npm start
+```
+
+Should open up in your browser.
--- easy_server.js
+++ easy_server.js
@@ -12,23 +12,23 @@ app.get('/index.html', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-app.use(express.static('static'));
+app.use(express.static(__dirname + '/static'));
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.post('/', function (req, res) {
-	
+
 	var jsonObject = JSON.stringify(req.body);
 	console.log(jsonObject);
   fs.writeFile(__dirname+"/static/post.json", jsonObject, function(err) {
     if(err) {
        return console.log(err);
     }
-    
+
     res.send('The file was saved successfully!');
-  }); 
+  });
 });
 
 app.listen(8888, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('Example app listening on port 8888!');
 });
